03be4e41d07f5cc4720930ba0696cba279fb9cf1
Gaurav Gurjar <ggurjar333@gmail.com>
2023-11-24 22:52:56 +0530

    Refactor get_transactions method to read and filter transactions from CSV

--- banking_app/database_service/database.py
+++ banking_app/database_service/database.py
@@ -1,3 +1,6 @@
+import csv
+# from database_service.database import Database
+
 class Database:
     _instance = None
 
@@ -8,5 +11,26 @@ class Database:
         return cls._instance
 
     def get_transactions(self, email, start_date, end_date):
-        # Logic to read and filter transactions from CSV
-        pass
+        transactions = []
+        with open('../../data/transactions.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                # Assuming the CSV file has columns: email, date, amount, description
+                if row[0] == email and start_date <= row[1] <= end_date:
+                    transactions.append(row)
+        return transactions
+
+# Import the Database class from the file
+
+# Create an instance of the Database class
+db = Database()
+
+# Call the get_transactions method
+email = "ggurjar333@gmail.com"
+start_date = "01-01-2023"
+end_date = "10-11-2023"
+transactions = db.get_transactions(email, start_date, end_date)
+
+# Print the transactions
+for transaction in transactions:
+    print(transaction)
