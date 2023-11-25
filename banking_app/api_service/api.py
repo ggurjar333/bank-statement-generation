2e11f999bd78ace956e1c3ee7647d279e1ee0e23
Gaurav Gurjar <ggurjar333@gmail.com>
2023-11-25 10:08:32 +0000

    updates apis

--- banking_app/api_service/api.py
+++ banking_app/api_service/api.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI, HTTPException
 from datetime import datetime
-from banking_app.database_service.database import Database
-from banking_app.pdf_generation_service.pdf_generator import PDFGenerator
-from banking_app.email_service.email_service import EmailService
+
+from database_service.database import Database
+from pdf_generation_service.pdf_generator import PDFGenerator
+from email_service.email_service import EmailService
 
 app = FastAPI()
-db = Database()  # Singleton instance
 
 @app.post("/generate_pdf")
-async def generate_pdf(email: str, start_date: datetime, end_date: datetime):
+async def generate_pdf(filename:str, email: str, start_date: datetime, end_date: datetime):
+    db = Database(filename, email, start_date, end_date)  # Singleton instance
     transactions = db.get_transactions(email, start_date, end_date)
-    pdf_content = PDFGenerator.generate_pdf(transactions)
-    EmailService.send_email(email, pdf_content)
+    # pdf_content = PDFGenerator.generate_pdf(transactions)
+    # EmailService.send_email(email, pdf_content)
+    PDFGenerator.generate_pdf(transactions)
     return {"message": "PDF generation and email sending initiated"}
--- banking_app/database_service/database.py
+++ banking_app/database_service/database.py
@@ -1,34 +1,72 @@
 import csv
-# from database_service.database import Database
+from datetime import datetime
 
 class Database:
     _instance = None
+    _transactions = None
 
-    def __new__(cls):
+    def __new__(cls, csv_file_path, email, start_date, end_date):
         if not cls._instance:
             cls._instance = super(Database, cls).__new__(cls)
-            # Initialize database connection or file reading here
+            cls._transactions = cls._instance._read_transactions(csv_file_path)
         return cls._instance
 
-    def get_transactions(self, email, start_date, end_date):
+    def _read_transactions(self, csv_file_path):
+        """
+        Read transactions from the CSV file.
+
+        Args:
+            csv_file_path (str): Path to the CSV file.
+
+        Returns:
+            list: List of all transactions from the CSV file.
+        """
         transactions = []
-        with open('../../data/transactions.csv', 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                # Assuming the CSV file has columns: email, date, amount, description
-                if row[0] == email and start_date <= row[1] <= end_date:
-                    transactions.append(row)
+        try:
+            with open(csv_file_path, 'r') as file:
+                reader = csv.DictReader(file)
+                transactions = list(reader)
+        except FileNotFoundError:
+            print("Error: File not found.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
         return transactions
+
+    def get_transactions(self, email, start_date, end_date):
+        """
+        Retrieve transactions for a specific email within a given date range.
+
+        Args:
+            email (str): The email for which transactions are retrieved.
+            start_date (str): The start date of the transaction range (format: "DD-MM-YYYY").
+            end_date (str): The end date of the transaction range (format: "DD-MM-YYYY").
+
+        Returns:
+            list: List of transactions matching the criteria.
+        """
+        filtered_transactions = []
+        # Convert date strings to datetime for comparison
+        start_date = datetime.strptime(start_date, "%d-%m-%Y")
+        end_date = datetime.strptime(end_date, "%d-%m-%Y")
+
+        for row in self._transactions:
+            row_date = datetime.strptime(row['date'], "%d-%m-%Y")
+            if row['email'] == email and start_date <= row_date <= end_date:
+                filtered_transactions.append(row)
+
+        return filtered_transactions
 
 # Import the Database class from the file
 
-# Create an instance of the Database class
-db = Database()
-
-# Call the get_transactions method
+# Create an instance of the Database class with file path and query parameters
+csv_file_path = 'transactions.csv'
 email = "ggurjar333@gmail.com"
 start_date = "01-01-2023"
 end_date = "10-11-2023"
+db = Database(csv_file_path, email, start_date, end_date)
+
+# Call the get_transactions method
 transactions = db.get_transactions(email, start_date, end_date)
 
 # Print the transactions
