8f82f22d26d66c0fff27065cf674284a32615cd2
Gaurav Gurjar <ggurjar333@gmail.com>
2023-11-25 10:16:36 +0000

    deletes unnecessary copies

--- banking_app2/email_service/email_service.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-
-class EmailService:
-    @staticmethod
-    def send_email(email, pdf_content):
-        # Logic to send email with PDF attachment using SMTP or other libraries
-        pass
